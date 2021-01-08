b2e50e2e97cbebec5b99f9f5b7bdc7becc08f8ac
Alina <alina88lis@gmail.com>
2021-01-08 03:01:36 +0300

    updated python_algorythms_9

--- python_algorythms_9/ex2.py
+++ python_algorythms_9/ex2.py
@@ -5,12 +5,13 @@
 
 import heapq
 from collections import Counter, namedtuple
+from binarytree import build
 
-
-class Node(namedtuple('Node', ['left', 'right'])):
+class Node1(namedtuple('Node', ['left', 'right'])):
     def walk(self, code, acc):
         self.left.walk(code, acc + '0')
         self.right.walk(code, acc + '1')
+
 
 
 class Leaf(namedtuple('Leaf', ['ch'])):
@@ -19,22 +20,33 @@ class Leaf(namedtuple('Leaf', ['ch'])):
 
 
 def huffman_encode(string):
-    h = []
+    hash_table = []
     for ch, frq in Counter(string).items():
-        h.append((frq, len(h), Leaf(ch)))
-    heapq.heapify(h)
-    print(h)
-    count = len(h)
-    while len(h) > 1:
-        frq1, _count1, left = heapq.heappop(h)
-        frq2, _count2, right = heapq.heappop(h)
-
-        heapq.heappush(h, (frq1 + frq2, count, Node(left, right)))
+        hash_table.append((frq, len(hash_table), Leaf(ch)))
+        print(hash_table)
+    heapq.heapify(hash_table)
+    count = len(hash_table)
+    while len(hash_table) > 1:
+        frq1, _count1, left = heapq.heappop(hash_table)
+        frq2, _count2, right = heapq.heappop(hash_table)
+        print(f'frq1 = {frq1}, _count1 = {_count1}, left = {left}')
+        print(f'frq2 = {frq2}, _count2 = {_count2}, right = {right}')
+        heapq.heappush(hash_table, (frq1 + frq2, count, Node1(left, right)))
         count += 1
     code = {}
-    if h:
-        [(_frq, _count, root)] = h
+    if hash_table:
+        [(_frq, _count, root)] = hash_table
+        print(f'[(_frq, _count, root)] = {[(_frq, _count, root)]}')
         root.walk(code, '')
-    return code
+
+    encoded_str = ''
+    for c in string:
+        encoded_str = encoded_str + ' ' + code[c]
+    return f'{string}\n{code}\n{encoded_str}'
+
 
 print(huffman_encode('abracadabra'))
+# с бумажки..........
+d = build([11, 5, 6, None, None, 2, 4, None, None, None, None, None, None, 2, 2, None, None, None, None, None,
+           None, None, None, None, None, None, None, 1, 1, None, None])
+print(d)
